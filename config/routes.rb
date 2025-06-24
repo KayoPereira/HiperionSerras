236e37cb6288c064b063bf56ffd477d1e727eeb2
KayoPereira <kayo.richard2000@gmail.com>
2025-06-24 20:27:17 -0300

    added banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   devise_for :users
-  root "pages#home"
-  # root "pages#construction"
+  # root "pages#home"
+  root "pages#construction"
+  get 'home', to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
