d9a023e51767cd1bad1a862b583bffc5d563fe35
KayoPereira <kayo.richard2000@gmail.com>
2025-06-24 19:30:23 -0300

    fixed navbar avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cpf, :phone, :address, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :cpf, :phone, :address, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
   end
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      my_pets_path
-    else
-      admin_root_path
+      root_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,50 @@ class User < ApplicationRecord
 
   has_many :pets
   has_one_attached :avatar
+
+  # Validações
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 50 }
+  validate :avatar_format
+
+  # Métodos úteis
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def display_name
+    full_name.present? ? full_name : email.split('@').first.capitalize
+  end
+
+  def avatar_url(size: :medium)
+    if avatar.attached?
+      case size
+      when :small
+        avatar.variant(resize_to_limit: [50, 50])
+      when :medium
+        avatar.variant(resize_to_limit: [150, 150])
+      when :large
+        avatar.variant(resize_to_limit: [300, 300])
+      else
+        avatar
+      end
+    else
+      # URL para avatar padrão se não houver imagem
+      "sem-imagem-avatar.png"
+    end
+  end
+
+  private
+
+  def avatar_format
+    return unless avatar.attached?
+
+    unless avatar.content_type.in?(%w[image/jpeg image/jpg image/png image/gif])
+      errors.add(:avatar, 'deve ser uma imagem (JPEG, PNG ou GIF)')
+    end
+
+    if avatar.byte_size > 5.megabytes
+      errors.add(:avatar, 'deve ter menos de 5MB')
+    end
+  end
 end
