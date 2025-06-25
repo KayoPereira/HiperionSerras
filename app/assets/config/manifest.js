7c18aa69c388ebf6ed9e7b55923631493185eb99
KayoPereira <kayo.richard2000@gmail.com>
2025-06-25 07:52:33 -0300

    corrigido assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,14 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .scss
 //= link application.js
-//= link bootstrap.js
-//= link @popperjs--core.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
 //= link application.css
-//= link controllers/active_controller.js
-//= link popper.js
-//= link bootstrap.min.js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
